fb5251d6e6a994088051379855334492f8b7eb11
Jenna Williams <jcwill415@gmail.com>
2020-08-13 12:59:41 -0400

    Remove views.py file

--- stock/quotes/forms.py
+++ stock/quotes/forms.py
@@ -1,7 +1,11 @@
+# Create a form for stock portfolio
+
+# import forms system for django
 from django import forms
 from .models import Stock
 
+# create class for stock form
 class StockForm(forms.ModelForm):
 	class Meta:
 		model = Stock
-		fields = ["ticker"]
+		fields = ["ticker"] # python list
